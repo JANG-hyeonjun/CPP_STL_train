d033e89ec1c0df29df702bc16b34e59abc5dc57b
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2022-05-28 12:01:39 +0900

    연관 컨테이너 공부

---
+++ chapter7_STL_RelatedContainer/Section01_set_insert/Section01_set_insert/Section01_set_insert.cpp
@@ -0,0 +1,39 @@
+﻿// Section01_set_insert.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <set>
+
+
+int main()
+{
+	std::set<int> s;
+
+	s.insert(50);
+	s.insert(30);
+	s.insert(80);
+	s.insert(40);
+	s.insert(10);
+	s.insert(70);
+	s.insert(90);
+
+	std::set<int>::iterator iter;
+	for (iter = s.begin(); iter != s.end(); ++iter)
+	{
+		std::cout << *iter << " ";
+	}
+	std::cout << std::endl;
+
+	s.insert(50); //중복된 원소(key)를 삽입한다. 실패!
+	s.insert(50);
+
+	for (iter = s.begin(); iter != s.end(); ++iter)
+	{
+		std::cout << *iter << " ";
+	}
+	std::cout << std::endl;
+
+
+	return 0;
+}
+
---
+++ chapter7_STL_RelatedContainer/Section_01_insert()의반환값/Section_01_insert()의반환값/Section_01_insert()의반환값.cpp
@@ -0,0 +1,46 @@
+﻿// Section_01_insert()의반환값.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <set>
+
+
+int main()
+{
+	std::set<int> s;
+	std::pair<std::set<int>::iterator, bool> pr;
+
+	pr = s.insert(50);
+	s.insert(40);
+	s.insert(80);
+
+	//set은 같은 원소(key)를 중복 저장할수 없으므로 insert()멤버 함수호출시 반환값으로 
+	//실패를 확인할 수 있습니다.
+	if (true == pr.second)
+		std::cout << *pr.first << "삽입 성공!" << std::endl;
+	else
+		std::cout << *pr.first << "가 이미 있습니다. 삽입 실패!" << std::endl;
+
+	std::set<int>::iterator iter;
+	for (iter = s.begin(); iter != s.end(); ++iter)
+	{
+		std::cout << *iter << " ";
+	}
+	std::cout << std::endl;
+
+	pr = s.insert(50);
+
+	if (true == pr.second)
+		std::cout << *pr.first << "삽입 성공!" << std::endl;
+	else
+		std::cout << *pr.first << "가 이미 있습니다. 삽입 실패!" << std::endl;
+
+	for (iter = s.begin(); iter != s.end(); ++iter)
+	{
+		std::cout << *iter << " ";
+	}
+	std::cout << std::endl;
+
+	return 0;
+}
+
